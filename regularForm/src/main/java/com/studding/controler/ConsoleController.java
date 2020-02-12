82ba8f77f936d14247150751c98002714d4f079b
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-02-12 18:51:44 +0200

    небольшие изменения

---
+++ regularForm/src/main/java/com/studding/controler/ConsoleController.java
@@ -0,0 +1,14 @@
+package com.studding.controler;
+
+public class ConsoleController {
+
+
+
+    public String concatenationString(String... message){
+        StringBuilder concatString = new StringBuilder();
+        for(String v : message) {
+            concatString = concatString.append(v);
+        }
+        return new String(concatString);
+    }
+}
